beab7e9cb9219f6ddea1d05707b1af6f04267be7
dell <jchhura21@gmail.com>
2021-12-25 19:49:21 +0530

    okay

--- app/src/main/java/com/example/prepsmartly/loginpage.kt
+++ app/src/main/java/com/example/prepsmartly/loginpage.kt
@@ -10,46 +10,36 @@ import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_loginpage.*
 
 class loginpage : AppCompatActivity() {
-    lateinit var firebaseAuth: FirebaseAuth
+    lateinit var firebaseAuth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loginpage)
-        firebaseAuth = FirebaseAuth.getInstance()
-        registertext.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
-        forgot.setOnClickListener {
-            val intent = Intent(this, forgotpasswordscreen::class.java)
-            startActivity(intent)
-        }
-
+        firebaseAuth= FirebaseAuth.getInstance()
+    registertext.setOnClickListener {
+        val intent= Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
+     forgot.setOnClickListener {
+         startActivity(Intent(this,forgotpasswordscreen::class.java))
+     }
         yoooo.setOnClickListener {
-            val email2 = emailid.editableText.toString().trim()
-            val password2 = passwordid.editableText.toString().trim()
-            if (emailid.text.isBlank()) {
-                Toast.makeText(this, "Email cannot be empty", Toast.LENGTH_SHORT).show()
-            } else if (passwordid.text.isBlank() || password2.length < 6) {
-                Toast.makeText(this, "Enter correct password", Toast.LENGTH_SHORT).show()
-            } else {
-                firebaseAuth.signInWithEmailAndPassword(email2, password2)
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Toast.makeText(this, "yeeee", Toast.LENGTH_SHORT).show()
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.exception)
-                            Toast.makeText(
-                                baseContext, "Authentication failed.",
-                                Toast.LENGTH_SHORT
-                            ).show()
+            val email2=emailid.editableText.toString().trim()
+            val password2=passwordid.editableText.toString().trim()
+            firebaseAuth.signInWithEmailAndPassword(email2, password2)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
 
-                        }
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+
                     }
-            }
-
-
+                }
         }
+
     }
 }
