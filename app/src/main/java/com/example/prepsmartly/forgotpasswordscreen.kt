d362c9bb0e9cbc4168bf4f64ee66e04aeb6b454b
dell <jchhura21@gmail.com>
2021-12-25 14:30:18 +0530

    done

---
+++ app/src/main/java/com/example/prepsmartly/forgotpasswordscreen.kt
@@ -0,0 +1,11 @@
+package com.example.prepsmartly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class forgotpasswordscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgotpasswordscreen)
+    }
+}
